40561de6a3c7b2556b189fa08249495d4f7c3a05
eduards-v <eduardsvagalsgti@gmail.com>
2018-03-26 17:01:56 +0100

    2) collatz-list function | return list of recursive values from a function

--- collatz-list.rkt
+++ collatz-list.rkt
@@ -1,13 +1,17 @@
 #lang racket
 
 (define (collatz-list x)
-  (odd? x)
-  )
+  (if (= x 1)
+      (cons x null)
+      (if (odd? x)
+          (cons x (collatz-list(+ (* x 3) 1)))
+          (cons x (collatz-list(/ x 2))))))
 
 (define (odd? x)
   (if (= (modulo x 2) 0)
       #f
       #t))
 
-(collatz-list 4)
 (collatz-list 5)
+(collatz-list 9)
+(collatz-list 2)
