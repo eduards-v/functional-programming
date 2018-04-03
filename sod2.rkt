3353616f4041e4d167a213e8c077595cec9238bc
eduards-v <eduardsvagalsgti@gmail.com>
2018-04-03 10:44:53 +0100

    9) sod2 function | coding and commenting function

--- sod2.rkt
+++ sod2.rkt
@@ -7,7 +7,7 @@
 (define (sod2 x y z)
   (if (null? x)
       null
-      (cons )))
+      (cons (odd? (car x)(car y)(car z))(sod2 (cdr x)(cdr y)(cdr z)))))
 
 ;   0 0 0
 ;   0 0 1 -> return 1
@@ -18,4 +18,16 @@
 ;   1 1 0
 ;   1 1 1 -> return 1
 
-(define (odd? x y z))
+; Check if x == y, if so result will be z.
+; Else check if z is 0 or 1,
+; return 0 if z == 1, else return 1.
+
+(define (odd? x y z)
+  (if(= x y)
+     z
+     (if(= z 1)
+        0
+        1)))
+
+(sod2 (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
+
